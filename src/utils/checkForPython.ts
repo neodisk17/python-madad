cf1e7a5d62e92c9526a0617687220fc89ee40cae
Lokesh Kumar <lk07540@gmail.com>
2023-12-31 09:19:39 +0530

    Added syncing of the packages

--- src/utils/checkForPython.ts
+++ src/utils/checkForPython.ts
@@ -1,32 +1,12 @@
-import { extensions, window } from 'vscode';
-import { PYTHON_EXTENSION_ID } from '../constant';
-import checkForPythonError from '../error-message/checkForPythonError';
+import { window } from 'vscode';
+import { getPythonPath } from './getPythonPath';
 
-const checkForPython = async (fileName: string) => {
-  const pythonExtension = extensions.getExtension(PYTHON_EXTENSION_ID);
-
-  if (!pythonExtension) {
-    window.showWarningMessage(checkForPythonError.pythonNotInstalled);
-    return;
-  }
-
-  if (!pythonExtension.activate) {
-    await pythonExtension.activate();
-  }
-
-  const pythonApi = pythonExtension?.exports;
-  const pythonPath = pythonApi.settings.getExecutionDetails().execCommand;
-
-  // This scenario will never occures, even the virtual environment is not available
-  // It will take the path of usr/bin/python
-
+export const checkForPython = async (fileName: string) => {
+  const pythonPath = await getPythonPath();
   if (!pythonPath) {
-    window.showWarningMessage(checkForPythonError.noInterpreterSelected);
     return;
   }
-
   const terminal = window.activeTerminal || window.createTerminal();
-
   const pythonCommand = `${pythonPath} -m pip install -r ${fileName};`;
   terminal.sendText(pythonCommand, true);
   terminal.show();
